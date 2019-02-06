38c2b66d2ea83e3f23009023d988238a0d5bb126
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2019-02-06 11:37:03 +0100

    Initial Commit

---
+++ Navigation Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation Controller
+//
+//  Created by Giuseppe Sapienza on 06/02/19.
+//  Copyright © 2019 Giuseppe Sapienza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
